07277463f211c8f43ea9b812a0f5d86045673867
itnd <1669232805@qq.com>
2023-01-16 23:31:20 +0800

    feat: 增加IconHover组件

---
+++ src/components/Modal/IconHover.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { IconHoverProps } from './interface';
+import cs from 'classnames';
+import './IconHover.less';
+
+const prefixCls = 'cutie-icon-hover';
+
+export default function IconHover(props: IconHoverProps) {
+  const { className, style, children, size = 'medium' } = props;
+
+  return (
+    <div
+      className={cs(prefixCls, className, {
+        [`${prefixCls}-size-${size}`]: size
+      })}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+}
